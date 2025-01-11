c2d203d6bfc89627a9b04372f7010d492f251a55
benjah05 <senzelebenjamine@gmail.com>
2025-01-11 01:54:04 -0800

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,19 @@
-#install a package
+# Install Python 3.8 using the system package manager
+package { 'python3.8':
+  ensure => present,
+}
 
-package { 'flask':
+# Install Flask using pip3
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3.8'],
 }
+
+# Install Werkzeug version compatible with Flask
+package { 'Werkzeug':
+  ensure   => '2.0.3',
+  provider => 'pip3',
+  require  => Package['Flask'],
+}
+
