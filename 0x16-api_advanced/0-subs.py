a167758baecf5bd283b69be127240b903170dba9
benjah05 <senzelebenjamine@gmail.com>
2025-03-08 11:15:14 -0800

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+find the number of subcribers in a subreddit
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'MyRedditApp/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return int(results.get("subscribers"))
+    """
+        get the number of subs in a subreddit
+    """
+    try:
+        users_api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        headers = {'User-Agent': 'MyRedditApp/1.0'}
+
+        user_requests = requests.get(users_api_url,
+                             headers=headers, allow_redirects=False)
+        user_requests.raise_for_status()
+        if 'application/json' not in user_requests.headers.get('Content-Type', ''):
+            return 0
+        user_data = user_requests.json()
+        if "data" not in user_data or "subscribers" not in user_data["data"]:
+            print("Error: 'data' or 'subscribers' key not found in response")
+            return 0
+        return int(user_data["data"]["subscribers"])
+    except requests.exceptions.HTTPError as err:
+        if user_requests.status_code == 404:
+            return 0
